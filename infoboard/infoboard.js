4c1560272965f2796c31450ef95f82dd37181f0c
pyabzz <>
2020-12-01 18:04:11 +0000

    Update infoboard

--- infoboard/infoboard.js
+++ infoboard/infoboard.js
@@ -1,10 +1,10 @@
 Infoboard = function (containerId, ...items) {
 	this.container = document.getElementById(containerId);
 	this.container.classList.add("infoboard")
-	this.items = items;
+	this.items = {};
 	const me = this;
 	items.forEach(item => {
-		const itemName = item[0];
+		const itemKey = item[0];
 		const initialValue = item[1] == null ? "" : item[1];
 
 		let itemElem = document.createElement('div');
@@ -12,17 +12,26 @@ Infoboard = function (containerId, ...items) {
 
 		let labelElem = document.createElement('span');
 		labelElem.classList.add("infoboard-label");
-		labelElem.innerText = itemName + ": ";
+		labelElem.innerText = itemKey + ": ";
 		itemElem.appendChild(labelElem);
 
 		let valueElem = document.createElement('span');
 		valueElem.classList.add("infoboard-value");
 		itemElem.appendChild(valueElem);
 
-		me["get" + itemName] = function () { return valueElem.innerText };
-		me["set" + itemName] = function (val) { valueElem.innerText = val };
+		me.items[itemKey] = {};
+		me.items[itemKey].get = function () { return valueElem.innerText };
+		me.items[itemKey].set = function (val) { valueElem.innerText = val };
 
-		me["set" + itemName](initialValue);
+		me.items[itemKey].set(initialValue);
 		me.container.appendChild(itemElem);
 	});
+}
+
+Infoboard.prototype.get = function (key) {
+	return this.items[key].get();
+}
+
+Infoboard.prototype.set = function (key, value) {
+	return this.items[key].set(value);
 }
