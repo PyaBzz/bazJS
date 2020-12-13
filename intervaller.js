3629858c24c71cf3c02fa4fbea57153820150e9a
pyabzz <>
2020-12-13 22:30:05 +0000

    Add more functionality to the intervaller

--- intervaller.js
+++ intervaller.js
@@ -1,30 +1,56 @@
-Intervaller = function (func, period = 1000) {
-	this.func = func;
-	this.loopId = 0;
-	this.period = period;
-}
+class Intervaller {
+	#func;
+	#period;
+	#periodStep;
+	#periodMin;
+	#loopId;
 
-Intervaller.prototype.run = function () {
-	this.loopId++;
-	let me = this;
-	this.loopHandle = setInterval(() => {
-		me.func();
-	}, me.period);
-}
+	constructor(func, period = 1000, periodStep = 10, periodMin = 1) {
+		this.#func = func;
+		this.#period = period;
+		this.#periodStep = periodStep;
+		this.#periodMin = periodMin;
+		this.#loopId = 0;
+	}
 
-Intervaller.prototype.stop = function () {
-	clearInterval(this.loopHandle);
-}
+	get #loopHandle() { return this['#runningLoop' + this.#loopId] }
+	set #loopHandle(val) { this['#runningLoop' + this.#loopId] = val }
+	get period() { return this.#period };
 
-Intervaller.prototype.setPeriod = function (p) {
-	this.stop();
-	this.period = p;
-	this.run();
-}
+	run() {
+		this.#loopId++;
+		let me = this;
+		this.#loopHandle = setInterval(() => {
+			me.#func();
+		}, me.#period);
+	}
 
-Object.defineProperties(Intervaller.prototype, {
-	loopHandle: {
-		get: function () { return this['runningLoop' + this.loopId]; },
-		set: function (val) { this['runningLoop' + this.loopId] = val; }
-	},
-});
+	stop() {
+		clearInterval(this.#loopHandle);
+	}
+
+	stepUp() {
+		const newPeriod = this.#period - this.#periodStep;
+		if (newPeriod >= this.#periodMin) {
+			this.stop();
+			this.#period = newPeriod;
+			this.run();
+		}
+	}
+
+	stepDown() {
+		this.stop();
+		this.#period += this.#periodStep;
+		this.run();
+	}
+
+	setPeriod(p) {
+		if (p >= this.#periodMin) {
+			this.stop();
+			this.#period = p;
+			this.run();
+		} else {
+			throw "Cannot set period below minimum";
+		}
+	}
+}
